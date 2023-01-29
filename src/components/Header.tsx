1b75b0baa1c2c1948d285160a6d55f6b12c59a2f
KhekhaevSelim <selim16695@mail.ru>
2023-01-29 21:38:28 +0300

    bootstrapic))

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="container">
+            <button type="button" className="btn btn-primary">Primary</button>
+            <button type="button" className="btn btn-secondary">Secondary</button>
+            <button type="button" className="btn btn-success">Success</button>
+        </div>
+    );
+};
+
+export default Header;
